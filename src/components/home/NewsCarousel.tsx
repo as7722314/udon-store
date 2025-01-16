052c3045a872e38fc12152b3c107a0dee9fc282d
jamespig <james7766646@gmail.com>
2025-01-16 17:05:08 +0800

    feat: 調整導覽點位置

--- src/components/home/NewsCarousel.tsx
+++ src/components/home/NewsCarousel.tsx
@@ -86,7 +86,7 @@ export default function NewsCarousel() {
       </button>
 
       {/* 導航點 */}
-      <div className="absolute -bottom-1 left-1/2 -translate-x-1/2 flex gap-3">
+      <div className="absolute -bottom-0 left-1/2 -translate-x-1/2 flex gap-3">
         {recentNews.map((_, index) => (
           <button
             key={index}
